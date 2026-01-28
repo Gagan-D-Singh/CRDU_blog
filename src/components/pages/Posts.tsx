c303dcb05ea15219ffe0808741cee7ec43e35a9e
Gagan <gagandeep.singh2@fiserv.com>
2026-01-28 19:26:31 +0530

    further updates

--- src/components/pages/Posts.tsx
+++ src/components/pages/Posts.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import { addPost } from "../../service/api";
+
 const Posts = () => {
+  const [newPost, setNewPost] = useState({ title: "", body: "" });
+
+  const handleAddPost = () => {
+    // Logic to add a new post will go here
+    const postData = async () => {
+      const response = await addPost(newPost);
+      console.log("Post added:", response.data);
+      setNewPost({ title: "", body: "" });
+    };
+    postData();
+  }
+
   return (
     <main>
       <h1>This will be responsible to add a new Post</h1>
+      <form className="post_form" onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="title">Title:</label>
+        <input type="text" id="title" name="title" value={newPost.title} onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}/>
+
+        <label htmlFor="body">Body:</label>
+        <textarea id="body" name="body" value={newPost.body} onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}></textarea>
+
+        <button type="submit" className="add_post_button" onClick={handleAddPost}>Add Post</button>
+      </form>
     </main>
   );
 };
--- src/service/api.ts
+++ src/service/api.ts
@@ -3,3 +3,7 @@ import axiosApi from "./axiosApi";
 export const getPosts : () => Promise<any> = () => {
     return axiosApi.get('/posts');
 }
+
+export const addPost : (postData: any) => Promise<any> = (postData) => {
+    return axiosApi.post('/posts', postData);
+}
